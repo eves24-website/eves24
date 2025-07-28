fea19e6218e691cdcfb20f8ec633f64fb2e7ef6f
eves24-website <eves24.website@gmail.com>
2025-07-28 15:12:28 +0530

    Update Products1.jsx

--- src/components/homes/home-handcraft/Products1.jsx
+++ src/components/homes/home-handcraft/Products1.jsx
@@ -8,7 +8,7 @@ import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Products1() {
-  const tabs = ["New Arrivals", "Best Sellers", "Deals"];
+  const tabs = ["New Arrivals", "Best Sellers", "Deals", "Deals", "Deals"];
   const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const {
