a2a9bde54bf2f8d5eddfce70046d7fc490443c2b
eves24-website <eves24.website@gmail.com>
2025-07-28 15:03:51 +0530

    Update Banner2.jsx

--- src/components/homes/home-plant/Banner2.jsx
+++ src/components/homes/home-plant/Banner2.jsx
@@ -9,21 +9,31 @@ export default function Banner2() {
           <div className="content-with-text wow fadeInUp">
             <div className="box-title-content">
               <span className="subtitle text-md fw-medium">
-                LIFE GATHERS AROUND PLANTS
+                Available at Wholesale Rates
               </span>
               <h2 className="title fw-semibold font-7">
-                Perfect Plants for Every Corner
+                 The Most Authentic Diamond Jewellery
               </h2>
               <p className="desc text-main text-md">
-                From small succulents to statement plants, find the ideal green
-                companion for any room. Explore our collection today!
+                Eves24 offers not just IGI Certified diamond Jewelry but we are pioneer in India to 
+                offer diamond jewelry Certified & SEALED by IGI in a temper proof pack. As soon as 
+                the sealed package is opened, the SEAL becomes VOID. So you are ensured that 
+                the package is opened only by you, after the certification is done by IGI.
+                IGI (International Gemological Institute) is the world's most trusted and 
+                reputed organization for checking the authenticity of diamond jewellery.
+              </p>
+              <p className="desc text-main text-md">
+               You can find IGI certificate number engraved on every piece of Jewellery & the authenticity
+                of jewellery can be cross verified on IGI's website (www.igi.org). Most important, IGI also
+                certifies the Diamonds used in the jewellery to be NATURAL. Beware of man-made laboratory 
+                grown CVD diamonds that have flooded the markets.
               </p>
             </div>
             <Link
               to={`/shop-default`}
               className="tf-btn bg-brown-14 animate-btn border-0"
             >
-              Shop Collection
+              BOOK AN APPOINTMENT
             </Link>
           </div>
           <div className="image-banner">
@@ -34,7 +44,7 @@ export default function Banner2() {
               >
                 <img
                   alt=""
-                  src="/images/banner/sub-plant-3.jpg"
+                  src="/images/banner/sub-tanishq-07.webp"
                   className=" ls-is-cached lazyloaded"
                   width={534}
                   height={543}
@@ -45,7 +55,7 @@ export default function Banner2() {
               <div className="shine-item img-style wow fadeInRight">
                 <img
                   alt=""
-                  src="/images/banner/plant-2.jpg"
+                  src="/images/banner/Tanishq-07.webp"
                   className=" ls-is-cached lazyloaded"
                   width={951}
                   height={951}
